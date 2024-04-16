de63f1b070ef2aefd1a5e6acfde936898c1b6d55
AggelosBarboutis <abarboutis@threenitas.com>
2024-04-16 20:12:25 +0300

    Sync commit

--- src/app/features/movies/movies.component.ts
+++ src/app/features/movies/movies.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { forkJoin } from 'rxjs';
 
 import { Movie } from '../../core/models/movies/movie.model';
 import { NowPlayingResponse } from '../../core/models/movies/responses/now-playing.model';
-import { PopularResponse } from '../../core/models/movies/responses/popular.model';
 import { TopRatedResponse } from '../../core/models/movies/responses/top-rated.model';
 import { TrendingResponse } from '../../core/models/movies/responses/trending.model';
 import { UpcomingResponse } from '../../core/models/movies/responses/upcoming.model';
@@ -19,11 +19,16 @@ import { PosterPathDirective } from '../../shared/directives/poster-path.directi
     styleUrl: './movies.component.scss',
 })
 export class MoviesComponent implements OnInit {
-    nowPlayingMovies: Array<Movie> = [];
-    popularMovies: Array<Movie> = [];
-    topRatedMovies: Array<Movie> = [];
-    trendingMovies: Array<Movie> = [];
-    upcomingMovies: Array<Movie> = [];
+    nowPlaying: Array<Movie> = [];
+    popular: Array<Movie> = [];
+    topRated: Array<Movie> = [];
+    trending: Array<Movie> = [];
+    upcoming: Array<Movie> = [];
+    trendingObservable = this.httpService.get<TrendingResponse>(this.urlService.urlFor(['trending', 'movie', 'day']));
+    nowPlayingObservable = this.httpService.get<NowPlayingResponse>(this.urlService.urlFor(['movie', 'now_playing']));
+    popularObservable = this.httpService.get<TopRatedResponse>(this.urlService.urlFor(['movie', 'top_rated']));
+    topRatedObservable = this.httpService.get<TopRatedResponse>(this.urlService.urlFor(['movie', 'top_rated']));
+    upcomingObservable = this.httpService.get<UpcomingResponse>(this.urlService.urlFor(['movie', 'upcoming']));
 
     constructor(
         private httpService: HttpClient,
@@ -31,28 +36,34 @@ export class MoviesComponent implements OnInit {
     ) {}
 
     ngOnInit(): void {
-        this.httpService
-            .get<NowPlayingResponse>(this.urlService.urlFor(['movie', 'now_playing']))
-            .subscribe((response) => {
-                this.nowPlayingMovies = response.results.slice(0, 10);
+        forkJoin([
+            this.trendingObservable,
+            this.nowPlayingObservable,
+            this.popularObservable,
+            this.topRatedObservable,
+            this.upcomingObservable,
+        ]).subscribe(([trending, nowPlaying, popular, topRated, upcoming]) => {
+            this.trending = trending.results.slice(0, 10);
+
+            nowPlaying.results.forEach((movie) => {
+                if (
+                    !this.trending.find((trendingMovie) => {
+                        console.log(trendingMovie);
+                        trendingMovie.id === movie.id;
+                    })
+                ) {
+                    this.nowPlaying.push(movie);
+                }
             });
+            console.log(this.nowPlaying);
 
-        this.httpService.get<PopularResponse>(this.urlService.urlFor(['movie', 'popular'])).subscribe((response) => {
-            this.popularMovies = response.results.slice(0, 10);
-        });
+            // this.nowPlaying = nowPlaying.results.slice(0, 10);
+            this.popular = popular.results.slice(0, 10);
+            this.topRated = topRated.results.slice(0, 10);
+            this.upcoming = upcoming.results.slice(0, 10);
 
-        this.httpService.get<TopRatedResponse>(this.urlService.urlFor(['movie', 'top_rated'])).subscribe((response) => {
-            this.topRatedMovies = response.results.slice(0, 10);
-        });
-
-        this.httpService
-            .get<TrendingResponse>(this.urlService.urlFor(['trending', 'movie', 'day']))
-            .subscribe((response) => {
-                this.trendingMovies = response.results.slice(0, 10);
-            });
-
-        this.httpService.get<UpcomingResponse>(this.urlService.urlFor(['movie', 'upcoming'])).subscribe((response) => {
-            this.upcomingMovies = response.results.slice(0, 10);
+            // console.log(this.trending[0]);
+            // console.log(this.nowPlaying[0]);
         });
     }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     baseUrl: 'https://api.themoviedb.org/3',
-    authToken: '', // You need to pass your TMDB authToken
+    authToken:
+        'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMmM1ZTFhNDEwOTM5MDM3YTcyMjE3MjljNzdiNzgwNiIsInN1YiI6IjY0ZjM4NGFmOTdhNGU2MDBjNDg3ZWY0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.W3q-blzsDM2S4eM9g-SIsQZ5RQUJhh0T0dbs0iXJ3j0', // You need to pass your TMDB authToken
 };
