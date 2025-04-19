17ab3f99114ceff24dccb595fe91af9ca6fe8fe9
Aggelos Barboutis <abarboutis@threenitas.com>
2025-04-20 01:12:40 +0300

    Adjust bottom nav bar padding for navigation bar

--- src/app/shared/components/mobile-navigation/mobile-navigation.component.ts
+++ src/app/shared/components/mobile-navigation/mobile-navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -8,29 +8,4 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
     templateUrl: './mobile-navigation.component.html',
     styleUrl: './mobile-navigation.component.scss',
 })
-export class MobileNavigationComponent implements OnInit {
-    hasBottomBar = false;
-
-    ngOnInit(): void {
-        this.checkBottomBar();
-    }
-
-    @HostListener('window:resize')
-    onResize(): void {
-        this.checkBottomBar();
-    }
-
-    private checkBottomBar(): void {
-        // Check if we're on a mobile device
-        if (window.innerWidth <= 768) {
-            // Check if there's a bottom bar by comparing viewport height with window height
-            const viewportHeight = window.innerHeight;
-            const windowHeight = window.outerHeight;
-
-            // If there's a significant difference, it likely means there's a bottom bar
-            this.hasBottomBar = windowHeight - viewportHeight > 50;
-        } else {
-            this.hasBottomBar = false;
-        }
-    }
-}
+export class MobileNavigationComponent {}
