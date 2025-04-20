fcfaf015cd4b21c40b9d80fa6c903aaa024142b8
Aggelos Barboutis <abarboutis@threenitas.com>
2025-04-20 03:47:08 +0300

    Make header fixed

--- src/app/shared/components/mobile-navigation/mobile-navigation.component.ts
+++ src/app/shared/components/mobile-navigation/mobile-navigation.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
+import { FadeInDirective } from '../../directives/fade-in.directive';
+
 @Component({
     selector: 'app-mobile-navigation',
     standalone: true,
-    imports: [RouterLink, RouterLinkActive],
+    imports: [RouterLink, RouterLinkActive, FadeInDirective],
     templateUrl: './mobile-navigation.component.html',
     styleUrl: './mobile-navigation.component.scss',
 })
