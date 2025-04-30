a63ab5074dceeba564a2ce09a20846a8c3034616
Aggelos Barboutis <abarboutis@threenitas.com>
2025-05-01 02:59:06 +0300

    Create person-list-item, reduce mobile paddings, adjust border-radius

--- src/app/shared/components/cast-person/cast-person.component.ts
+++ src/app/shared/components/cast-person/cast-person.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { DEFAULT } from '../../constants/defaults.constant';
+import { FadeInDirective } from '../../directives/fade-in.directive';
 import { ProfilePathDirective } from '../../directives/profile-path.directive';
 import { ProfileSize } from '../../enumerations/profile-size.enum';
 import { MediaCreditsCastPerson } from '../../models/media.model';
@@ -10,7 +11,7 @@ import { MediaCreditsCastPerson } from '../../models/media.model';
 @Component({
     selector: 'app-cast-person',
     standalone: true,
-    imports: [CommonModule, ProfilePathDirective, RouterLink],
+    imports: [CommonModule, ProfilePathDirective, RouterLink, FadeInDirective],
     templateUrl: './cast-person.component.html',
     styleUrl: './cast-person.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
